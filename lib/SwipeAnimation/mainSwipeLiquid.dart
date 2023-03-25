93c4283cd945707baaddc6c5cea5985a18e89b29
Liudmyla Telesyk <liudmyla.teleshyk@gmail.com>
2023-03-24 21:04:48 -0400

    new Swipe Liquid

--- lib/SwipeAnimation/mainSwipeLiquid.dart
+++ lib/SwipeAnimation/mainSwipeLiquid.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:neon_widgets/neon_widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,23 +20,38 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final pages = [
     Container(
-        child: Stack (
-          children: [
-            Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(image: NetworkImage("https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-                    fit: BoxFit.cover,
-                  ),
-                )
-            ),
-            Center(
-              child: Text(
-                'Page 1',
-                style: TextStyle(fontSize: 40, color: Colors.white),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+              "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      // ),
+      child: Center(
+        // child: Column(
+        //     children: [
+        //       NeonSearchBar(),
+              child: FlickerNeonText(
+                text: "Page 1",
+                flickerTimeInMilliSeconds: 600,
+                spreadColor: Colors.blue,
+                blurRadius: 20,
+                textSize: 74,
               ),
-            ),
-          ],
-        )
+
+          // NeonLeftMsgCard(
+          //   lightBlurRadius: 20,
+          //   lightSpreadRadius: 5,
+          //   msg: "Page 1",
+          //   time: "10:45",
+          // ),
+          // const SizedBox(
+          //   height: 20,
+          // ),
+        // ]
+        // ),
+      ),
     ),
     Container(
       color: Colors.blue,
@@ -145,5 +161,3 @@ class MyHomePage extends StatelessWidget {
 //     );
 //   }
 // }
-
-
